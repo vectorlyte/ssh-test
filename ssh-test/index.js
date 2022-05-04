0f302e8ac3c80534ec676d835b807fa6456a2cd3
Armand I <vectorlite@gmail.com>
2022-05-03 20:31:44 -0400

    added fueling
    cars are now objects in an array that get a fuel boolean attatched to them in the gasStation function

--- ssh-test/index.js
+++ ssh-test/index.js
@@ -1,13 +1,15 @@
 const x = 123;
 const y = 2;
 
-const cars = ["red car", "blue car", "yellow car"];
+const cars = [{name: "red car"}, 
+{name: "blue car"}, {name: "yellow car"};
 const isFueled = true;
 
 function gasStation() {
     let i = 0;
     do{
         if(isFueled){
+            cars[i].isFueled = true;
             console.log(cars[i] + " is has a full tank of gas and is ready to go!");
         }
         i++;
